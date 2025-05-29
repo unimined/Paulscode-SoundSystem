212c3962c6049199079932bbe48a8ce61d63ba10
Caroline Joy Bell <caroline@halotroop.com>
2025-05-29 15:14:05 -0700

    Fix most warnings and spelling issues

--- src/main/java/paulscode/sound/IStreamListener.java
+++ src/main/java/paulscode/sound/IStreamListener.java
@@ -1,11 +1,39 @@
+/*
+ * The SoundSystem License:
+ *
+ * You are free to use this library for any purpose, commercial or otherwise.
+ * You may modify this library or source code, and distribute it any way you
+ * like, provided the following conditions are met:
+ *
+ * 1) You may not falsely claim to be the author of this library or any
+ *    unmodified portion of it.
+ * 2) You may not copyright this library or a modified version of it and then
+ *    sue me for copyright infringement.
+ * 3) If you modify the source code, you must clearly document the changes
+ *    made before redistributing the modified source code, so other users know
+ *    it is not the original code.
+ * 4) You are not required to give me credit for this library in any derived
+ *    work, but if you do, you must also mention my website:
+ *    https://www.paulscode.com
+ * 5) I the author will not be responsible for any damages (physical,
+ *    financial, or otherwise) caused by the use if this library or any part
+ *    of it.
+ * 6) I the author do not guarantee, warrant, or make any representations,
+ *    either expressed or implied, regarding the use of this library or any
+ *    part of it.
+ *
+ * Author: Paul Lamb
+ * https://www.paulscode.com
+ */
+
 package paulscode.sound;
 
-public interface IStreamListener
-{
-    /**
-     * Notifies implementation that an End Of Stream was reached.
-     * @param sourcename String identifier of the source which reached the EOS.
-     * @param queueSize Number of items left the the stream's play queue, or zero if none.
-    */
-    public void endOfStream( String sourcename, int queueSize );
+public interface IStreamListener {
+	/**
+	 * Notifies implementation that an End Of Stream was reached.
+	 *
+	 * @param sourceName String identifier of the source which reached the EOS.
+	 * @param queueSize  Number of items left in the stream's play queue, or zero if none.
+	 */
+	void endOfStream(String sourceName, int queueSize);
 }
